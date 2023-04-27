1b30e0ddce0a39f0c32bc28b55a476542fd5b7c0
chiduso <chiduso.arinze@gmail.com>
2023-04-28 00:26:45 +0100

    Aizen
    Yokoso watashi no soul society

---
+++ assets/js/script.js
@@ -0,0 +1,48 @@
+//  let insert = document.getElementById('insert'),    
+//     yo = document.getElementById('yo');
+ 
+//  function calc(val){
+//   return insert.innerText  +=  val.innerText
+//  };
+
+//  function clear(){
+//   let display = insert.innerText;
+//   insert.innerText = '';
+//   return  display
+//  };a
+// // yo.addEventListener('click', (e) => {
+// //   e.preventDefault();
+// //   insert.innerText += calc(yo);
+// // })
+
+const display = document.querySelector('#display');
+const buttons = document.querySelectorAll('button');
+
+buttons.forEach((item) => {
+  item.onclick = () => {
+    if (item.id == 'clear') {
+      display.innerText = '';
+    }else if (item.id == 'backspace') {
+      let string = display.innerText.toString();
+      display.innerText = string.substr(0, string.length - 1);      
+    }else if (display.innerText != '' && item.id == 'equal') {
+      display.innerText = eval(display.innerText);
+    }else if (display.innerText == '' && item.id == 'equal') {
+      display.innerText = 'Empty!'
+      setTimeout(() => (display.innerText = ''), 2000);
+    }else{
+      display.innerText += item.id;
+    }
+  }
+})
+
+const themeToggleBtn = document.querySelector('.theme-toggler');
+const calculator = document.querySelector('.calculato');
+const toggleIcon = document.querySelector('.toggler-icon');
+let isDark = true;
+themeToggleBtn.onclick = () => {
+  calculator.classList.toggle('dark');
+  themeToggleBtn.classList.toggle('active');
+  isDark = !isDark;
+}
+
